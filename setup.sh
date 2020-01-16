fbd39f07220cd7063e5b72a04ae2a92c63a1dc51
Shannon Pamperl <shanman190@gmail.com>
2020-01-16 15:25:47 -0600

    Enable symlinks when cloning dotfiles for local support

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/shanman190/dotfiles.git "${HOME}/.dotfiles/"
+git clone -c core.symlinks=true https://github.com/shanman190/dotfiles.git "${HOME}/.dotfiles/"
 
 read -r -p "Do you have a local dotfiles repo to include? [no] " answer
 if [[ -z ${answer} || "${answer}" == "yes" || "${answer}" == "y" ]]; then
