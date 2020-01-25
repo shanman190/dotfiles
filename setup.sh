1d62ddf423696649096d0cdde068b70600213fb0
Shannon Pamperl <shanman190@gmail.com>
2020-01-25 12:18:46 -0600

    Fix up local dotfiles check

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 git clone -c core.symlinks=true https://github.com/shanman190/dotfiles.git "${HOME}/.dotfiles/"
 
 read -r -p "Do you have a local dotfiles repo to include? [no] " answer
-if [[ -z ${answer} || "${answer}" == "yes" || "${answer}" == "y" ]]; then
+if [[ -n ${answer} && ("${answer}" == "yes" || "${answer}" == "y") ]]; then
 	read -r -p "Clone url? " localrepo
 	git clone ${localrepo} "${HOME}/.localdotfiles/"
 fi
